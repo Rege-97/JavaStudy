7d4f1d4a2b6a3e1d38e8cf337db717f0733c4467
Rege-97 <cogus1027@gmail.com>
2024-12-09 21:21:01 +0900

    Study : 2024.12.09 -1

---
+++ day01/src/day01/Day01_01.java
@@ -0,0 +1,12 @@
+package day01;
+
+import java.lang.*;
+
+public class Day01_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이클립스로 작성한 문자열~~");
+	}
+
+}
