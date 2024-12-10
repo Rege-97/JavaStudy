0e31a2e272d767937a472ca6273601b59c9aa849
Rege-97 <cogus1027@gmail.com>
2024-12-10 15:47:39 +0900

    study : 2024.12.10 -3

--- day02/src/day02/Day02_05.java
+++ day02/src/day02/Day02_05.java
@@ -12,7 +12,9 @@ public class Day02_05 {
 		System.out.println("result = " + result); // 출력
 
 		char ch = 'A' + 1; // A의 아스키코드 값 65에서 +1 하여 66인 B 저장
-		System.out.println("ch = " + (char) (ch + 1)); // 문자에 +1을 하여 int로 출력되어 67 출력 - char로 형변환
+		System.out.println("ch = " + (char) (ch + 1)); 
+		// 문자에 정수 1을 더하여 int 데이터로 변환되어 67 출력 - char로 형변환해야 C가 출력
+		
 	}
 
 }
