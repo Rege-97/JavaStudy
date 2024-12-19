1ee5b15a75096ca466371286e575c924a0b01221
Rege-97 <cogus1027@gmail.com>
2024-12-19 11:18:20 +0900

    Study : 2024.12.19 -1

---
+++ day09/src/day09/Day09_01.java
@@ -0,0 +1,13 @@
+package day09;
+
+public class Day09_01 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			int temp = (int) (Math.random() * 6) + 1;
+
+			System.out.print(temp + " ");
+		}
+	}
+
+}
---
+++ day09/src/day09/Day09_02.java
@@ -0,0 +1,91 @@
+package day09;
+
+import java.io.*;
+
+public class Day09_02 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int user = 0;
+		int com = 0;
+
+		String user_s = "보";
+		String com_s = "보";
+
+		while (true) {
+			System.out.println("============================");
+			System.out.println("  가위 바위 보 게임 v1.0 ");
+			System.out.println("----------------------------");
+			System.out.println(" 1.가위 2.바위 3.보 4.종료 ");
+			System.out.println("============================");
+
+			do {
+				System.out.print("메뉴 > ");
+				user = Integer.parseInt(br.readLine());
+				com = (int) (Math.random() * 3 + 1);
+				System.out.println();
+
+				if (user >= 5 || user <= 0) {
+					System.out.println("잘못 입력하였습니다.");
+				} else if (user <= 3) {
+					break;
+				}
+
+			} while (user != 4);
+
+			if (user == 4) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+
+			if (user == 1) {
+				user_s = "가위";
+			} else if (user == 2) {
+				user_s = "바위";
+			} else {
+				user_s = "보";
+			}
+
+			if (com == 1) {
+				com_s = "가위";
+			} else if (com == 2) {
+				com_s = "바위";
+			} else {
+				com_s = "보";
+			}
+
+			System.out.println("사용자 : " + user + "." + user_s);
+			System.out.println("컴퓨터 : " + com + "." + com_s);
+			System.out.println();
+
+			if (user == 1) {
+				if (com == 1) {
+					System.out.println("비겼습니다.");
+				} else if (com == 2) {
+					System.out.println("컴퓨터 승!");
+				} else {
+					System.out.println("사용자 승!");
+				}
+			} else if (user == 2) {
+				if (com == 1) {
+					System.out.println("사용자 승!");
+				} else if (com == 2) {
+					System.out.println("비겼습니다.");
+				} else {
+					System.out.println("컴퓨터 승!");
+				}
+			} else if (user == 3) {
+				if (com == 1) {
+					System.out.println("컴퓨터 승!");
+				} else if (com == 2) {
+					System.out.println("사용자 승!");
+				} else {
+					System.out.println("비겼습니다.");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
