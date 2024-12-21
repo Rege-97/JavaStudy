75b299059d7b9a41155db0cea23db4b734180cf6
Rege-97 <cogus1027@gmail.com>
2024-12-21 17:07:13 +0900

    Study : 2024.12.21 -1

---
+++ day11/src/day11/Day11_01.java
@@ -0,0 +1,40 @@
+package day11;
+
+import java.io.*;
+
+public class Day11_01 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int arr[] = new int[5];
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(i + "번 점수 : ");
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		System.out.println("-----------------------");
+
+		int temp = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+}
